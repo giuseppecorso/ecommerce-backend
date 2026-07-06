e3ee44210f0a9e33437a5dd5203373348eff65f4
Giuseppe Corso <giuseppecorso91@gmail.com>
2026-07-06 18:59:58 +0200

    Update status message

--- src/main/java/com/giuseppe/ecommerce/HelloController.java
+++ src/main/java/com/giuseppe/ecommerce/HelloController.java
@@ -13,6 +13,6 @@ public class HelloController {
 
     @GetMapping("/status")
     public String status() {
-        return "E-commerce API is running";
+        return "Start of new E-Commerce";
     }
 }
