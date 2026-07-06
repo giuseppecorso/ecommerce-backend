d688ac631bb8933a17996551a42320aee67fb849
Giuseppe Corso <giuseppecorso91@gmail.com>
2026-07-06 17:11:03 +0200

    Initial commit: Spring Boot skeleton with hello and status endpoints

---
+++ src/main/java/com/giuseppe/ecommerce/HelloController.java
@@ -0,0 +1,18 @@
+package com.giuseppe.ecommerce;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello World!";
+    }
+
+    @GetMapping("/status")
+    public String status() {
+        return "E-commerce API is running";
+    }
+}
