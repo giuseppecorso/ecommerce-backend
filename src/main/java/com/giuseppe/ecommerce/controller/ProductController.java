14db0c390116419a572ff1475e63dac37c9ca223
Giuseppe Corso <giuseppecorso91@gmail.com>
2026-07-08 10:30:06 +0200

    Add ProductRepository and endpoint to list products

--- src/main/java/com/giuseppe/ecommerce/controller/ProductController.java
+++ src/main/java/com/giuseppe/ecommerce/controller/ProductController.java
@@ -1,12 +1,20 @@
 package com.giuseppe.ecommerce.controller;
 
 import com.giuseppe.ecommerce.model.Product;
+import com.giuseppe.ecommerce.repository.ProductRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.math.BigDecimal;
+import java.util.List;
 
 @RestController
 public class ProductController {
+
+    private final ProductRepository repository;
+
+    public ProductController(ProductRepository repository) {
+        this.repository = repository;
+    }
 
     @GetMapping("/api/products/demo")
     public Product getProduct() {
@@ -16,4 +24,11 @@ public class ProductController {
         String description = "T-Shirt Nike stile Vintage colore bianco con stampa";
         return new Product(id, name, description, price, 10);
     }
+
+    @GetMapping("/api/products")
+    public List<Product> getProducts() {
+        return repository.findAll();
+    }
+
+
 }
